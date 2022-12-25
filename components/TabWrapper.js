aa82ba17eafaceddd117459430d43d6d131fe93c
mihkuno <caindayjoeninyo@gmail.com>
2022-12-26 05:30:25 +0800

    added tab switch screen transition

--- components/TabWrapper.js
+++ components/TabWrapper.js
@@ -1,37 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, Text, View, Dimensions, Layout } from 'react-native';
+import Animated, { FadeInLeft, FadeInRight } from 'react-native-reanimated';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import { TabBar } from './TabBar';
 
-const styles = StyleSheet.create({
-	screen: {
-		width: '100%',
-		height: '100%',
-		flex: 6,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#2f3542',
-	},
-	text: { color: 'white' },
-});
+const tabs = ['home', 'map', 'map-pin', 'bookmark', 'user'];
+let prevSelectedTab = 0;
 
-const DemoScreen = ({ route }) => (
-	<View style={styles.screen}>
-		<Text style={styles.text}>{route.name}</Text>
-	</View>
-);
+const DemoScreen = ({ route }) => {
+	const styles = StyleSheet.create({
+		screen: {
+			width: '100%',
+			height: '100%',
+			flex: 6,
+			justifyContent: 'center',
+			alignItems: 'center',
+			backgroundColor: 'whitesmoke',
+		},
+		text: { color: '#222' },
+	});
+
+	const currSelectedTab = tabs.indexOf(route.name);
+
+	const transitionDirection =
+		currSelectedTab > prevSelectedTab ? FadeInRight : FadeInLeft;
+
+	prevSelectedTab = currSelectedTab;
+
+	return (
+		<Animated.View style={styles.screen} entering={transitionDirection}>
+			<Text style={styles.text}>{route.name}</Text>
+		</Animated.View>
+	);
+};
+
+const screenOptions = {
+	unmountOnBlur: true,
+	headerShown: false,
+};
+
+const sceneContainerStyle = {
+	backgroundColor: 'whitesmoke',
+};
 
 export const TabWrapper = () => {
 	const Tab = createBottomTabNavigator();
 	return (
 		<NavigationContainer>
 			<View style={{ flex: 1, position: 'relative' }}>
-				<Tab.Navigator tabBar={props => <TabBar {...props} />}>
-					<Tab.Screen name="home" component={DemoScreen} />
-					<Tab.Screen name="map" component={DemoScreen} />
-					<Tab.Screen name="map-pin" component={DemoScreen} />
-					<Tab.Screen name="bookmark" component={DemoScreen} />
-					<Tab.Screen name="user" component={DemoScreen} />
+				<Tab.Navigator
+					{...{ screenOptions, sceneContainerStyle }}
+					tabBar={props => <TabBar {...props} />}
+				>
+					<Tab.Screen name={tabs[0]} component={DemoScreen} />
+					<Tab.Screen name={tabs[1]} component={DemoScreen} />
+					<Tab.Screen name={tabs[2]} component={DemoScreen} />
+					<Tab.Screen name={tabs[3]} component={DemoScreen} />
+					<Tab.Screen name={tabs[4]} component={DemoScreen} />
 				</Tab.Navigator>
 			</View>
 		</NavigationContainer>
