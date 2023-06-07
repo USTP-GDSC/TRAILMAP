71f857542f9ede7624227c4d04d9a0e66d14d8e9
mihkuno <caindayjoeninyo@gmail.com>
2023-06-07 08:01:03 +0800

    added search and json data

--- include/SearchBar.jsx
+++ include/SearchBar.jsx
@@ -1,31 +1,141 @@
 import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { View, TextInput, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-const SearchBar = () => {
+const SearchBar = ({_DATA}) => {
   const [searchText, setSearchText] = useState('');
 
   const handleSearch = (text) => {
     setSearchText(text);
+    
+
+    const search = (user_input) => {
+        // split characters not ascii
+        /*
+            [^...] is a negated character set that matches any character not present in the set.
+            \w matches any word character (letters, digits, and underscores).
+            \s matches any whitespace character (spaces, tabs, line breaks, etc.).
+            \d matches any digit character (0-9).
+        */
+        const regex = /[()\s_\-,\[\]?]+/;
+
+        // convert input to lowercase
+        user_input = user_input.toLowerCase().trim().split(regex);
+
+        // remove unnecessary keywords in query
+        const removeWords = ['of', 'the', 'and', 'in', 'for', 'on', ''];
+        user_input = user_input.filter(word => !removeWords.includes(word));
+
+
+
+        const countStringsInArray = (array, searchArray) => {
+          let count = 0;
+          
+          for (let i = 0; i < array.length; i++) {
+              const currentString = array[i];
+              
+              if (searchArray.includes(currentString)) {
+              count++;
+              }
+          }
+          
+          return count;
+        }
+      
+
+
+        let results = [];
+
+
+
+        for (const bldg_id in _DATA) {
+
+            // check building names
+            const bldg_name = _DATA[bldg_id].name;
+            const bldg_split = bldg_name.toLowerCase().split(regex);
+            let matchCount = countStringsInArray(bldg_split, user_input);
+
+            if (matchCount > 0) {
+                results.push(
+                    {
+                        rank: matchCount, 
+                        bldg: bldg_id,
+                        name: bldg_name
+                    }
+                );
+            }
+            
+
+            // check room names
+            const rooms = _DATA[bldg_id].rooms;
+            for (const floor_id in rooms) {
+                const floor_rooms = rooms[floor_id];
+
+                // split the user_input and find each individual keywords   
+                for (const room_name of floor_rooms) {
+                    
+                    const room_split = room_name.toLowerCase().split(regex);
+
+                    matchCount = countStringsInArray(room_split, user_input);
+
+                    if (matchCount > 0) {
+                        results.push(
+                            {
+                                rank: matchCount, 
+                                floor: floor_id,
+                                bldg: bldg_id,
+                                room: room_name, 
+                            }
+                        );
+                    }
+                }
+
+            }
+
+
+        }
+
+
+        // Filter the two largest rank values
+        if (results.length >= 2) {
+            // Sort the results array in descending order based on rank
+            results.sort((a, b) => b.rank - a.rank);
+
+            let filteredResults;
+            // if the rank is one less difference, then just ignore it
+            if (results[0].rank - results[1].rank === 1) {
+                filteredResults = results.slice(0, 1);
+            }
+            else {
+                filteredResults = results.filter((result) => result.rank >= results[1].rank);
+            }
+
+            return filteredResults;
+        }
+        else {
+            return results;
+        }
+    }
+
+    console.warn(search(text));
+
     // Perform search or filtering logic here
     // You can use the 'text' value to filter/search data
   };
 
-  const handleSearchButton = () => {
-    // Perform search or filtering logic here using the 'searchText' state
-  };
-
   return (
     <View style={styles.container}>
+      {searchText === '' ? (
+        <View style={styles.searchIconContainer}>
+          <Icon name="search" size={24} color="gray" />
+        </View>
+      ) : null}
       <TextInput
         style={styles.input}
         placeholder="Search"
         value={searchText}
         onChangeText={handleSearch}
       />
-      <TouchableOpacity style={styles.searchButton} onPress={handleSearchButton}>
-        <Feather name="search" size={24} color="black" />
-      </TouchableOpacity>
     </View>
   );
 };
@@ -44,8 +154,8 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
   },
-  searchButton: {
-    paddingHorizontal: 10,
+  searchIconContainer: {
+    marginRight: 10,
   },
 });
 
--- screens/NavigateScreen.jsx
+++ screens/NavigateScreen.jsx
@@ -1,10 +1,88 @@
-import { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { useState, useLayoutEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useAssets, Asset } from 'expo-asset';
+import { WebView } from 'react-native-webview';
 
-import TrailMap from '../content/TrailMap';
+import search_data from '../assets/data.json';
 import SearchBar from '../include/SearchBar';
 
 export default NavigateScreen = ({onBuildingClicked}) => {
+
+	/*manage webview*/
+	const [html, loadHTML] = useState(``);
+
+	const [scripts] = useAssets([
+		require(`../assets/sketch/library/Three.jsr`),
+		require(`../assets/sketch/library/Control.jsr`),
+		require(`../assets/sketch/library/Loader.jsr`),
+		require(`../assets/sketch/_preload.jsr`),
+		require(`../assets/sketch/_initialize.jsr`),
+		require(`../assets/sketch/_animate.jsr`),
+	]);
+
+		useLayoutEffect(() => {
+		if (html || !scripts) return;
+
+		loadHTML(
+			`
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<meta charset="utf-8" />
+				<meta 
+					name="viewport" 
+					content="width=device-width, 
+							initial-scale=1.0" />
+				<style>
+					html,
+					body {
+						margin: 0;
+						padding: 0;
+						position: fixed;
+					}
+				</style>
+			</head>
+			<body>
+				${scripts.map(
+					(o) => `<script type="text/javascript" src="${o.localUri}"></script>`)
+					.join('\n')
+				}
+			</body>
+			</html>
+			`
+		);
+	});
+
+	const handleListener = event => {
+		if (event.nativeEvent.data.startsWith("onBuildingClick")) {
+			// console.warn(event.nativeEvent.data.substr(17));
+			onBuildingClicked(<Text>{event.nativeEvent.data.substr(17)}</Text>);
+			// console.warn(handleMapUpdate);
+		}
+	};
+
+	const webViewProps = {
+		style: { backgroundColor: '#D5D9E6' },
+
+		bounces: false,
+		scrollEnabled: false,
+		overScrollMode: 'never',
+		javaScriptEnabled: true,
+		setBuiltInZoomControls: false,
+		androidLayerType: 'hardware',
+
+		originWhitelist: ['*'],
+		allowFileAccess: true,
+		domStorageEnabled: true,
+		mixedContentMode: 'always',
+		allowUniversalAccessFromFileURLs: true,
+
+		source: { html },
+		onMessage: handleListener,		
+	};
+
+
+	/*manage this screen*/
 	const [mount, shouldRemount] = useState(false);
 	const [controlLock, setControlLock] = useState('auto');
 
@@ -50,11 +128,17 @@ export default NavigateScreen = ({onBuildingClicked}) => {
 		},
 	};
 
+
 	return (
 		<View style={{ flex: 1, position: 'relative' }} {...manageControlProps}>
-			<TrailMap onBuildingClicked={onBuildingClicked} />
+			<WebView {...webViewProps} 
+				injectedJavaScript={`
+					// JavaScript code that will be executed in the WebView
+					// You can use window.ReactNativeWebView.postMessage() to send messages back to React Native
+				`}
+				/>
 			<View style={styles.searchBarContainer}>
-			<SearchBar />
+			<SearchBar _DATA={search_data}/>
 		</View>
 	  </View>
 	);
@@ -67,7 +151,6 @@ const styles = StyleSheet.create({
 	  left: 10,
 	  right: 10,
 	  zIndex: 1, // Ensure the search bar appears on top of other elements
-
 	},
   });
   
