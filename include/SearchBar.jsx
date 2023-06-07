13e11b9ffc3c7aee23a03f1f66b97aef13269db9
mihkuno <caindayjoeninyo@gmail.com>
2023-06-07 14:33:20 +0800

    added async search bar

--- include/SearchBar.jsx
+++ include/SearchBar.jsx
@@ -2,125 +2,134 @@ import React, { useState } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
-const SearchBar = ({_DATA}) => {
+const SearchBar = ({ _DATA }) => {
   const [searchText, setSearchText] = useState('');
 
   const handleSearch = (text) => {
     setSearchText(text);
+
+    searchAsync(text, (results) => {
+      // Perform the callback logic with the search results
+      console.log(results);
+    });
+  };
+
+  const searchAsync = (text, callback) => {
+   
+    
+    const search = (user_input) => {
+      // split characters not ascii
+      /*
+          [^...] is a negated character set that matches any character not present in the set.
+          \w matches any word character (letters, digits, and underscores).
+          \s matches any whitespace character (spaces, tabs, line breaks, etc.).
+          \d matches any digit character (0-9).
+      */
+      const regex = /[()\s_\-,\[\]?]+/;
+
+      // convert input to lowercase
+      user_input = user_input.toLowerCase().trim().split(regex);
+
+      // remove unnecessary keywords in query
+      const removeWords = ['of', 'the', 'and', 'in', 'for', 'on', ''];
+      user_input = user_input.filter(word => !removeWords.includes(word));
+
+
+
+      const countStringsInArray = (array, searchArray) => {
+        let count = 0;
+        
+        for (let i = 0; i < array.length; i++) {
+            const currentString = array[i];
+            
+            if (searchArray.includes(currentString)) {
+            count++;
+            }
+        }
+        
+        return count;
+      }
     
 
-    const search = (user_input) => {
-        // split characters not ascii
-        /*
-            [^...] is a negated character set that matches any character not present in the set.
-            \w matches any word character (letters, digits, and underscores).
-            \s matches any whitespace character (spaces, tabs, line breaks, etc.).
-            \d matches any digit character (0-9).
-        */
-        const regex = /[()\s_\-,\[\]?]+/;
 
-        // convert input to lowercase
-        user_input = user_input.toLowerCase().trim().split(regex);
-
-        // remove unnecessary keywords in query
-        const removeWords = ['of', 'the', 'and', 'in', 'for', 'on', ''];
-        user_input = user_input.filter(word => !removeWords.includes(word));
+      let results = [];
 
 
 
-        const countStringsInArray = (array, searchArray) => {
-          let count = 0;
-          
-          for (let i = 0; i < array.length; i++) {
-              const currentString = array[i];
-              
-              if (searchArray.includes(currentString)) {
-              count++;
-              }
+      for (const bldg_id in _DATA) {
+
+          // check building names
+          const bldg_name = _DATA[bldg_id].name;
+          const bldg_split = bldg_name.toLowerCase().split(regex);
+          let matchCount = countStringsInArray(bldg_split, user_input);
+
+          if (matchCount > 0) {
+              results.push(
+                  {
+                      rank: matchCount, 
+                      bldg: bldg_id,
+                      name: bldg_name
+                  }
+              );
           }
           
-          return count;
-        }
-      
+
+          // check room names
+          const rooms = _DATA[bldg_id].rooms;
+          for (const floor_id in rooms) {
+              const floor_rooms = rooms[floor_id];
+
+              // split the user_input and find each individual keywords   
+              for (const room_name of floor_rooms) {
+                  
+                  const room_split = room_name.toLowerCase().split(regex);
+
+                  matchCount = countStringsInArray(room_split, user_input);
+
+                  if (matchCount > 0) {
+                      results.push(
+                          {
+                              rank: matchCount, 
+                              floor: floor_id,
+                              bldg: bldg_id,
+                              room: room_name, 
+                          }
+                      );
+                  }
+              }
+
+          }
 
 
-        let results = [];
+      }
 
 
+      // Filter the two largest rank values
+      if (results.length >= 2) {
+          // Sort the results array in descending order based on rank
+          results.sort((a, b) => b.rank - a.rank);
 
-        for (const bldg_id in _DATA) {
+          let filteredResults;
+          // if the rank is one less difference, then just ignore it
+          if (results[0].rank - results[1].rank === 1) {
+              filteredResults = results.slice(0, 1);
+          }
+          else {
+              filteredResults = results.filter((result) => result.rank >= results[1].rank);
+          }
 
-            // check building names
-            const bldg_name = _DATA[bldg_id].name;
-            const bldg_split = bldg_name.toLowerCase().split(regex);
-            let matchCount = countStringsInArray(bldg_split, user_input);
-
-            if (matchCount > 0) {
-                results.push(
-                    {
-                        rank: matchCount, 
-                        bldg: bldg_id,
-                        name: bldg_name
-                    }
-                );
-            }
-            
-
-            // check room names
-            const rooms = _DATA[bldg_id].rooms;
-            for (const floor_id in rooms) {
-                const floor_rooms = rooms[floor_id];
-
-                // split the user_input and find each individual keywords   
-                for (const room_name of floor_rooms) {
-                    
-                    const room_split = room_name.toLowerCase().split(regex);
-
-                    matchCount = countStringsInArray(room_split, user_input);
-
-                    if (matchCount > 0) {
-                        results.push(
-                            {
-                                rank: matchCount, 
-                                floor: floor_id,
-                                bldg: bldg_id,
-                                room: room_name, 
-                            }
-                        );
-                    }
-                }
-
-            }
-
-
-        }
-
-
-        // Filter the two largest rank values
-        if (results.length >= 2) {
-            // Sort the results array in descending order based on rank
-            results.sort((a, b) => b.rank - a.rank);
-
-            let filteredResults;
-            // if the rank is one less difference, then just ignore it
-            if (results[0].rank - results[1].rank === 1) {
-                filteredResults = results.slice(0, 1);
-            }
-            else {
-                filteredResults = results.filter((result) => result.rank >= results[1].rank);
-            }
-
-            return filteredResults;
-        }
-        else {
-            return results;
-        }
+          return filteredResults;
+      }
+      else {
+          return results;
+      }
     }
 
-    console.warn(search(text));
-
-    // Perform search or filtering logic here
-    // You can use the 'text' value to filter/search data
+    // Execute the search logic asynchronously
+    setTimeout(() => {
+      const results = search(text);
+      callback(results);
+    }, 0);
   };
 
   return (
@@ -160,3 +169,14 @@ const styles = StyleSheet.create({
 });
 
 export default SearchBar;
+
+
+
+
+
+
+
+
+
+
+
